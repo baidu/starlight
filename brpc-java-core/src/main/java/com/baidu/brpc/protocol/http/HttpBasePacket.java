374285d1d02a79a23ae117297105eb0bdfb43a03
motu <motu@pinduoduo.com>
2019-01-18 00:11:28 +0800

    add Packet interface

--- brpc-java-core/src/main/java/com/baidu/brpc/protocol/http/HttpBasePacket.java
+++ brpc-java-core/src/main/java/com/baidu/brpc/protocol/http/HttpBasePacket.java
@@ -2,11 +2,6 @@ package com.baidu.brpc.protocol.http;
 
 import com.baidu.brpc.protocol.Packet;
 
-/**
- * @author 柳汝滕 fingthinking@qq.com
- * @version v0.1
- * @date 2019年01月13日 9:56 AM
- */
 public class HttpBasePacket implements Packet {
     // Do nothing
 }
