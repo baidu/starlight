3a6de1a86b9e0101e5185822f17ef1db94c3fe90
GuoHao <guohaoice@gmail.com>
2019-04-03 10:23:05 +0800

    Optimize example project's scripts and packaging configuration (#40)

--- brpc-java-examples/src/main/bin/run_http_server.sh
+++ brpc-java-examples/src/main/bin/run_http_server.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
+cd `dirname $0`
+PROJECT_DIR=`pwd`/../../../
+cd ${PROJECT_DIR}
 
-export JVM_OPTIONS=" -server -Xmn2g -Xmx6g -Xms6g -Xss256k -Xverify:none \
+LOG_DIR="./logs"
+if [[ ! -d ${LOG_DIR} ]];then
+    mkdir ${LOG_DIR}
+fi
+
+JVM_OPTIONS=" -server -Xmn2g -Xmx6g -Xms6g -Xss256k -Xverify:none \
  -XX:+DisableExplicitGC -XX:+AlwaysPreTouch \
  -XX:+AggressiveOpts -XX:AutoBoxCacheMax=20000 \
  -XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSInitiatingOccupancyOnly \
  -XX:+UseParNewGC -XX:+UseConcMarkSweepGC -XX:ParallelGCThreads=4 \
- -XX:+PrintGCDetails  -XX:+PrintGCTimeStamps -Xloggc:./logs/server_gc.log "
+ -XX:+PrintGCDetails  -XX:+PrintGCTimeStamps -Xloggc:./logs/http_server_gc.log "
 
-export CUSTOM_CLASSPATH="target/classes:target/test-classes:target/dependency/* "
+CUSTOM_CLASSPATH="target/classes:target/test-classes:target/dependency/* "
 
-export MAIN_CLASS="com.baidu.brpc.example.http.proto.RpcServerTest"
+MAIN_CLASS="com.baidu.brpc.example.http.proto.RpcServerTest"
 
-java $JVM_OPTIONS -cp $CUSTOM_CLASSPATH $MAIN_CLASS
+java ${JVM_OPTIONS} -cp ${CUSTOM_CLASSPATH} ${MAIN_CLASS}
