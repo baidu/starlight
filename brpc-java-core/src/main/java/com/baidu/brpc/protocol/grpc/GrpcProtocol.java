2eb1193e8956627cf097779b36b0013419896bf8
kowaywang <827765236@qq.com>
2019-06-04 22:32:23 +0700

    create GrpcProtocol file

---
+++ brpc-java-core/src/main/java/com/baidu/brpc/protocol/grpc/GrpcProtocol.java
@@ -0,0 +1,42 @@
+package com.baidu.brpc.protocol.grpc;
+
+import com.baidu.brpc.buffer.DynamicCompositeByteBuf;
+import com.baidu.brpc.exceptions.BadSchemaException;
+import com.baidu.brpc.exceptions.NotEnoughDataException;
+import com.baidu.brpc.exceptions.TooBigDataException;
+import com.baidu.brpc.protocol.AbstractProtocol;
+import com.baidu.brpc.protocol.Request;
+import com.baidu.brpc.protocol.Response;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * Support grpc protocol in brpc
+ */
+public class GrpcProtocol extends AbstractProtocol {
+
+    @Override
+    public Object decode(ChannelHandlerContext ctx, DynamicCompositeByteBuf in, boolean isDecodingRequest) throws BadSchemaException, TooBigDataException, NotEnoughDataException {
+        return null;
+    }
+
+    @Override
+    public ByteBuf encodeRequest(Request request) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Response decodeResponse(Object msg, ChannelHandlerContext ctx) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Request decodeRequest(Object packet) throws Exception {
+        return null;
+    }
+
+    @Override
+    public ByteBuf encodeResponse(Request request, Response response) throws Exception {
+        return null;
+    }
+}
