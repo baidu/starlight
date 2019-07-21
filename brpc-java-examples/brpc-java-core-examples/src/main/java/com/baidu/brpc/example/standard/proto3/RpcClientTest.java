8f2a9571c132dbc9a1dbf411c4951693de9fce41
jinyuxin <810916965@qq.com>
2019-07-21 21:05:17 +0800

    support protobuf3 for the first time.

---
+++ brpc-java-examples/brpc-java-core-examples/src/main/java/com/baidu/brpc/example/standard/proto3/RpcClientTest.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2018 Baidu, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.baidu.brpc.example.standard.proto3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import com.baidu.brpc.client.BrpcProxy;
+import com.baidu.brpc.client.RpcCallback;
+import com.baidu.brpc.client.RpcClient;
+import com.baidu.brpc.client.RpcClientOptions;
+import com.baidu.brpc.client.loadbalance.LoadBalanceStrategy;
+import com.baidu.brpc.example.interceptor.CustomInterceptor;
+import com.baidu.brpc.exceptions.RpcException;
+import com.baidu.brpc.interceptor.Interceptor;
+import com.baidu.brpc.protocol.Options;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by wenweihu86 on 2017/4/26.
+ */
+@SuppressWarnings("unchecked")
+@Slf4j
+public class RpcClientTest {
+
+    public static void main(String[] args) {
+        RpcClientOptions clientOption = new RpcClientOptions();
+        clientOption.setProtocolType(Options.ProtocolType.PROTOCOL_BAIDU_STD_VALUE);
+        clientOption.setWriteTimeoutMillis(1000);
+        clientOption.setReadTimeoutMillis(1000);
+        clientOption.setMaxTotalConnections(1000);
+        clientOption.setMinIdleConnections(10);
+        clientOption.setLoadBalanceType(LoadBalanceStrategy.LOAD_BALANCE_FAIR);
+        clientOption.setCompressType(Options.CompressType.COMPRESS_TYPE_NONE);
+
+        String serviceUrl = "list://127.0.0.1:8002";
+//        String serviceUrl = "zookeeper://127.0.0.1:2181";
+        if (args.length == 1) {
+            serviceUrl = args[0];
+        }
+
+        // build request
+        Echo3.EchoRequest request = Echo3.EchoRequest.newBuilder()
+                .setMessage("Hello World from client!")
+                .build();
+
+        List<Interceptor> interceptors = new ArrayList<Interceptor>();
+        interceptors.add(new CustomInterceptor());
+
+        // sync call
+        RpcClient rpcClient = new RpcClient(serviceUrl, clientOption, interceptors);
+//        RpcClient rpcClient = new RpcClient(serviceUrl, clientOption, interceptors);
+        EchoService echoService = BrpcProxy.getProxy(rpcClient, EchoService.class);
+        try {
+            Echo3.EchoResponse response = echoService.echo(request);
+            System.out.printf("sync call service=EchoService.echo success, "
+                            + "request=%s,response=%s\n",
+                    request.getMessage(), response.getMessage());
+        } catch (RpcException ex) {
+            log.warn("sync call failed, ex=", ex);
+        }
+        rpcClient.stop();
+
+        // async call
+        rpcClient = new RpcClient(serviceUrl, clientOption, interceptors);
+//        rpcClient = new RpcClient(serviceUrl, clientOption, interceptors);
+        RpcCallback callback = new RpcCallback<Echo3.EchoResponse>() {
+            @Override
+            public void success(Echo3.EchoResponse response) {
+                System.out.printf("async call EchoService.echo success, response=%s\n",
+                        response.getMessage());
+            }
+
+            @Override
+            public void fail(Throwable e) {
+                System.out.printf("async call EchoService.echo failed, %s\n", e.getMessage());
+            }
+        };
+        EchoServiceAsync asyncEchoService = BrpcProxy.getProxy(rpcClient, EchoServiceAsync.class);
+        try {
+            Future<Echo3.EchoResponse> future = asyncEchoService.echo(request, callback);
+            try {
+                Echo3.EchoResponse response = future.get(1000, TimeUnit.MILLISECONDS);
+                System.out.println("response from future:" + response.getMessage());
+            } catch (Exception ex1) {
+                ex1.printStackTrace();
+            }
+        } catch (RpcException ex) {
+            System.out.println("rpc send failed, ex=" + ex.getMessage());
+        }
+        rpcClient.stop();
+    }
+
+}
