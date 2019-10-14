33c25f92543357c18adf0bdafeaedad1dab42926
Hao Guan <guanhao@baidu.com>
2019-10-14 16:59:51 +0800

    Sets CorrelationId on Response. (#134)
    Client side interceptor has access to the Correlation ID now.

--- brpc-java-core/src/main/java/com/baidu/brpc/interceptor/LoadBalanceInterceptor.java
+++ brpc-java-core/src/main/java/com/baidu/brpc/interceptor/LoadBalanceInterceptor.java
@@ -1,11 +1,22 @@
 /*
- * Copyright (C) 2019 Baidu, Inc. All Rights Reserved.
+ * Copyright (c) 2019 Baidu, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.baidu.brpc.interceptor;
 
 import com.baidu.brpc.ChannelInfo;
-import com.baidu.brpc.client.AsyncAwareFuture;
 import com.baidu.brpc.client.RpcClient;
 import com.baidu.brpc.client.RpcFuture;
 import com.baidu.brpc.client.channel.BrpcChannel;
@@ -74,12 +85,13 @@ public class LoadBalanceInterceptor extends AbstractInterceptor {
 
     protected void rpcCore(Request request, Response response) throws Exception {
         // send request with the channel.
-        AsyncAwareFuture future = rpcClient.sendRequest(request);
+        RpcFuture future = (RpcFuture) rpcClient.sendRequest(request);
         // receive
         if (future.isAsync()) {
-            response.setRpcFuture((RpcFuture) future);
+            response.setRpcFuture(future);
         } else {
             response.setResult(future.get(request.getReadTimeoutMillis(), TimeUnit.MILLISECONDS));
+            response.setCorrelationId(future.getCorrelationId());
         }
     }
 }
