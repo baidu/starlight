c000ec12151240d1ca5f11b964ac18104d8f1adb
wangqingpeng <wangqingpeng@baidu.com>
2020-06-17 17:38:08 +0800

    1. add test for http path

--- brpc-java-examples/brpc-java-core-examples/src/main/java/com/baidu/brpc/example/http/json/EchoService.java
+++ brpc-java-examples/brpc-java-core-examples/src/main/java/com/baidu/brpc/example/http/json/EchoService.java
@@ -1,6 +1,9 @@
 package com.baidu.brpc.example.http.json;
 
+import com.baidu.brpc.protocol.BrpcMeta;
+
 public interface EchoService {
+    @BrpcMeta(serviceName = "test/hei", methodName = "abc")
     String hello(String request);
     String hello2(String userName, Integer userId);
 }
